965ab007f41abae85809db6d46cf11f8a15e151a
Ben Shyong <likeachild@gmail.com>
2011-05-28 11:41:59 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Medhub::Application.config.secret_token = '4e8d4b90fe4bbbac5e53cf142a1efa1eb1720d6d5f733d3b100257e47c4219d74ec890446a1846bd1f28980933717ab475c532b1098c0cac5512a5b88dd5e98a'
